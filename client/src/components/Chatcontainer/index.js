e68986520f3386e1d42d85c34f7f0435cd745c0a
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-28 13:32:52 -0400

    proper unsubscribe

--- client/src/components/Chatcontainer/index.js
+++ client/src/components/Chatcontainer/index.js
@@ -3,39 +3,32 @@ import styles from "./ChatContainer.module.scss";
 import ChatWindow from "./ChatWindow";
 import ChatInput from "./ChatInput";
 
-const mockMSG = {
-  user: "0xc9b6628b0C44fe39170CFFCc3bd2cbECf15F7B5e",
-  msg: "Hello Worlds!",
-  timestamp: 2345432,
-  mined: true
-};
-const mockChats = [mockMSG, mockMSG, mockMSG];
-
 export default class ChatContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { messages: [], ...props };
   }
 
+  unsubscribe = null;
+
   componentDidMount = async () => {
-    await this.getAllMesages();
-    const subscription = await this.subscribeToEvents();
+    await this.getAllMsg();
+    this.unsubscribe = await this.subscribeToMessages();
   };
 
-  subscribeToEvents = async () => {
-    const { accounts, instance, web3 } = this.props;
-    let messages = [];
-
+  subscribeToMessages = async () => {
+    const { instance } = this.props;
     const subscription = await instance.events.message().on("data", event => {
-      this.getAllMesages();
+      this.getAllMsg();
     });
 
     return subscription;
   };
 
-  getAllMesages = async (count = 10) => {
-    const { instance, web3 } = this.props;
+  getAllMsg = async () => {
+    const { instance } = this.props;
     let messages = [];
+
     const logs = await instance.getPastEvents("message", {
       fromBlock: 0,
       toBlock: "latest"
@@ -46,13 +39,18 @@ export default class ChatContainer extends Component {
       messages.push({ message, timestamp, user });
     });
 
-    this.setState((state, props) => {
+    this.setState(() => {
       return { messages: messages };
     });
   };
 
+  componentWillUnmount = () => {
+    if (this.unsubscribe) {
+      this.unsubscribe.unsubscribe();
+    }
+  };
+
   render() {
-   // console.log(this.state);
     return (
       <div className={styles.chatContainer}>
         <ChatWindow messages={this.state.messages} {...this.props} />
