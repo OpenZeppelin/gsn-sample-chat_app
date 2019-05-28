132044f3a6cdecd25665c08f38434a37467e7b7d
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-28 14:51:14 -0400

    update GSN accept relayed and post relayed call.

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -1,11 +1,30 @@
 pragma solidity ^0.5.0;
 
-contract ChatApp {
+import "tabookey-gasless/contracts/RelayRecipient.sol";
+import "zos-lib/contracts/Initializable.sol";
+
+contract ChatApp is RelayRecipient, Initializable{
     
     event message(string message, address user, uint timestamp);
 
     function postMessage(string memory _msg) public {
         emit message(_msg, msg.sender, now);
     }
+
+    function init_hub(RelayHub hub_addr) public {
+    init_relay_hub(hub_addr);
+    }
+
+    function accept_relayed_call(address /*relay*/, address /*from*/,
+    bytes memory/*encoded_function*/, uint /*gas_price*/, 
+    uint /*transaction_fee*/ ) public view returns(uint32) {
+    return 0; // accept everything.
+}
+    // nothing to be done post-call.
+    // still, we must implement this method.
+    function post_relayed_call(address /*relay*/, address /*from*/,
+    bytes memory /*encoded_function*/, bool /*success*/,
+    uint /*used_gas*/, uint /*transaction_fee*/ ) public {
+}
     
 }
