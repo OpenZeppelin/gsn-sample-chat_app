bfaa11a37966e4d9d47e4e26f0191e6a2a8779ec
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-28 15:34:48 -0400

    Gassless Relay works, but getting a broadcastTX error.

--- client/src/components/Chatcontainer/ChatInput/index.js
+++ client/src/components/Chatcontainer/ChatInput/index.js
@@ -16,7 +16,7 @@ export default class ChatInput extends Component {
     this.setState({ validated: true,
     value: '' });
     const tx = await this.instance.postMessage(this.state.value).send({from: this.accounts[0]});
-    console.log(tx)
+    console.log("The Returned Transactions: ", tx);
   };
 
   handleValidation = e => {
