fbd065287d7d28c5bc9d068a5a63293eb905bf43
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-07-24 16:33:06 -0400

    Add tooltip to Buttons

--- client/src/components/GSNContainer/index.js
+++ client/src/components/GSNContainer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "rimble-ui";
+import { Button, Tooltip } from "rimble-ui";
 import styles from "./GSNContainer.module.scss";
 
 export default class GSNContainer extends Component {
@@ -21,15 +21,19 @@ export default class GSNContainer extends Component {
   render() {
     return (
       <div className={styles.button}>
+        <Tooltip message="Sign your transaction inside MetaMask in the traditional way." placement="top">
         <Button size="small" onClick={() => this.setProvider("MetaMask")}>
           MetaMask
-        </Button>
+        </Button></Tooltip>
+        <Tooltip message="Use MetaMask to sign a message for the GSN Relay but not to sign a transaction." placement="top">
         <Button size="small" onClick={() => this.setProvider("MMSigner")}>
           MM Signer
-        </Button>
+        </Button></Tooltip>
+        <Tooltip message="Use a keypair, generated in the browser, to sign a message for the GSN Relay. No MetaMask involved!" placement="top">
         <Button size="small" onClick={() => this.setProvider("Ephemeral")}>
           Ephemeral
         </Button>
+        </Tooltip>
       </div>
     );
   }
