2ecd897933b1c57c49289bc5fedb4cf0196ba944
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-07-25 16:59:53 -0400

    Fund meta mask button shows balance

--- client/src/components/fundMetaMask/index.js
+++ client/src/components/fundMetaMask/index.js
@@ -2,20 +2,54 @@ import React, { Component } from "react";
 import { Button } from "rimble-ui";
 
 export default class FundMetaMask extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { balance: null };
+  }
+
+  componentDidMount = async () => {
+    this.poll = setInterval(() => this.getBalance(), 1000);
+  
+  };
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+    this.timer=null;
+  }
+
+  getBalance = async () => {
+    const { web3, accounts } = this.props;
+    let balance;
+
+    if (web3) {
+      try {
+        balance = await web3.eth.getBalance(accounts[0]);
+        balance = web3.utils.fromWei(balance, "ether");
+        this.setState({ balance });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   fund = async () => {
     const { ganacheAccounts, accounts, ganacheWeb3 } = this.props;
-    await ganacheWeb3.eth.sendTransaction({
+
+    const tx = await ganacheWeb3.eth.sendTransaction({
       from: ganacheAccounts[0],
       to: accounts[0],
       value: 2e18
     });
+    if (tx) {
+      this.getBalance();
+    }
   };
 
   render() {
     return (
       <div>
         <Button size="small" onClick={() => this.fund()}>
-          Fund Meta Mask
+          Fund Meta Mask: (balance: {this.state.balance})
         </Button>
       </div>
     );
