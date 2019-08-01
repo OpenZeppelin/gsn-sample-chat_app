a227718b61136978b0fbcba4c0a8f33cf057f501
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-08-01 17:48:48 -0400

    Work with or without web3 injected.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import {
   useWeb3Injected,
   useEphemeralKey,
+  useWeb3Network
 } from "@openzeppelin/network";
 
 import { Loader } from "rimble-ui";
@@ -9,7 +10,6 @@ import { Loader } from "rimble-ui";
 import ChatContainer from "./components/Chatcontainer/index";
 import styles from "./App.module.scss";
 import logo from "../src/images/OZ_logo.png";
-
 
 const relayHubAddress = "0x9C57C0F1965D225951FE1B2618C92Eefd687654F";
 
@@ -297,23 +297,40 @@ const RelayHubAbi = [
 ];
 
 const App = () => {
-
+  const prod = false;
   const signKey = useEphemeralKey();
 
   const gasPrice = 22000000001;
   let relay_client_config = {
     txfee: process.env.REACT_APP_TX_FEE || 90,
-    force_gasPrice: gasPrice,            //override requested gas price
+    force_gasPrice: gasPrice, //override requested gas price
     gasPrice: gasPrice, //override requested gas price
-    force_gasLimit: 500000,        //override requested gas limit.
+    force_gasLimit: 500000, //override requested gas limit.
     gasLimit: 500000, //override requested gas limit.
     verbose: true
   };
-  
-  const web3Context = useWeb3Injected({
-    gsn: { signKey, ...relay_client_config }
-  });
-  
+
+  let web3Context = null; //
+
+  if (window.ethereum) {
+    web3Context = useWeb3Injected({
+      gsn: { signKey, ...relay_client_config }
+    });
+  } else {
+    if (prod) {
+      web3Context = useWeb3Network(
+        "https://rinkeby.infura.io/v3/d6760e62b67f4937ba1ea2691046f06d",
+        {
+          gsn: { signKey, ...relay_client_config }
+        }
+      );
+    } else {
+      web3Context = useWeb3Network("http://127.0.0.1:8545", {
+        gsn: { signKey, ...relay_client_config }
+      });
+    }
+  }
+
   const defaultState = {
     web3Context: web3Context,
     chatAppInstance: null,
@@ -324,9 +341,9 @@ const App = () => {
   const [state, setState] = useState(defaultState);
   const [fetchState, setFetchState] = useState({ fetching: false });
 
-  const setFetching = (value) => {
-    setFetchState({fetching: value})
-  }
+  const setFetching = value => {
+    setFetchState({ fetching: value });
+  };
 
   useEffect(() => {
     const { lib, networkId } = web3Context;
@@ -374,7 +391,11 @@ const App = () => {
           <img src={logo} alt="Logo" />
         </div>
         <h1>GSN Chat APP</h1>
-        <ChatContainer {...state} fetchState={fetchState} setFetchState={setFetching} />
+        <ChatContainer
+          {...state}
+          fetchState={fetchState}
+          setFetchState={setFetching}
+        />
       </div>
     );
   };
