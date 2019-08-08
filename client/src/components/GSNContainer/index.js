030552305ec118221b9d672c6118441343e3a2e7
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-08-08 11:05:46 -0400

    Clean up Advanced options

--- client/src/components/GSNContainer/index.js
+++ client/src/components/GSNContainer/index.js
@@ -5,19 +5,8 @@ import { useWeb3Injected } from "@openzeppelin/network";
 import axios from "axios";
 
 const GSNContainer = props => {
-  const { ChatAppAbi } = props;
   const [toggleState, setToggleState] = useState(false);
 
-  let injected = null;
-  try {
-    injected = useWeb3Injected();
-    injected.requestAuth();
-  } catch (error) {
-    //No injected web3
-    injected = null;
-  }
-
-  const [state, setState] = useState({ instance: null });
   const [relayState, setRelayState] = useState({
     RelayServerAddress: null,
     MinGasPrice: null,
@@ -26,25 +15,12 @@ const GSNContainer = props => {
   });
 
   useEffect(() => {
-    let instance = null;
-    if (injected) {
-      instance = new injected.lib.eth.Contract(
-        ChatAppAbi.abi,
-        props.chatAppInstance._address
-      );
-    }
-
-    setState({ instance });
-  }, [injected]);
-
-  useEffect(() => {
     axios.get("https://rinkeby-01.gsn.openzeppelin.org/getaddr").then(res => {
       const result = res.data;
       console.log(result);
       setRelayState(result);
     });
   }, []);
-
 
   if (toggleState) {
     console.log("Relay info:", relayState);
@@ -63,9 +39,7 @@ const GSNContainer = props => {
             <div className={styles.small}>
               Relay Ready: {relayState.Ready ? "True" : "False"}
             </div>
-            <div className={styles.small}>
-              RelayServerAddress: 
-            </div>
+            <div className={styles.small}>RelayServerAddress:</div>
             <div className={styles.small}>{relayState.RelayServerAddress}</div>
             <div className={styles.small}>
               Minimum Gas Price: {relayState.MinGasPrice}
@@ -78,11 +52,18 @@ const GSNContainer = props => {
           <div className={styles.small}>{props.signKey.address}</div>
           <div className={styles.smallBold}>Contract Address:</div>{" "}
           <div className={styles.small}>{props.chatAppInstance._address}</div>
-          {injected && injected.connected ? (
-            <Button size="small" onClick={() => window.open(`https://gsn.openzeppelin.com/recipients/${props.chatAppInstance._address}`)}>
-              Top up Contract
-            </Button>
-          ) : null}
+          <Button
+            size="small"
+            onClick={() =>
+              window.open(
+                `https://gsn.openzeppelin.com/recipients/${
+                  props.chatAppInstance._address
+                }`
+              )
+            }
+          >
+            Top up Contract
+          </Button>
         </div>
       </div>
     );
